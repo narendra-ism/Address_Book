8c26a3bacafcfb563544ad1370c5bfecafa31bc1
narendra-ism <narendra11d@gmail.com>
2019-01-09 10:24:23 +0530

    DID ALL OF THINKS

--- address_book/urls.py
+++ address_book/urls.py
@@ -4,4 +4,6 @@ from django.urls import path
 urlpatterns = [
     path('',views.home,name="home"),
     path('add_address',views.add_address,name="add_address"),
+    path('edit/<list_id>',views.edit, name="edit"),
+    path('delete/<list_id>',views.delete, name="delete"),
 ]
